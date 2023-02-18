f707ed434deb18352a95485d3d3faae92d2e8345
Valentin <pamerra@mail.ru>
2023-02-18 13:18:32 +0300

    Initial Commit

---
+++ VideoProGitHub/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VideoProGitHub
+//
+//  Created by MacBook on 18.02.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
